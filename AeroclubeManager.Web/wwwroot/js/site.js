21cf52af6b2e08716d62480de48d2e1e9168cdf9
tiago <tiago@VIVOBOOK_15>
2024-08-11 15:48:10 -0300

    Adicionar arquivos de projeto.

---
+++ AeroclubeManager.Web/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿var menuButton = document.querySelector('.menuButton')
+var manuNav = document.querySelector('nav')
+
+menuButton.addEventListener('click', () => {
+    manuNav.classList.toggle('open')
+    menuButton.classList.toggle('close')
+})
